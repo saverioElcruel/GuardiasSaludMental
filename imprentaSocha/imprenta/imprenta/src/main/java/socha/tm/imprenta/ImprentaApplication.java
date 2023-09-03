6e833086a5926176ff4eb8236b9ce4576325ba08
Tomas Manoukian <tomanoukian@gmail.com>
2023-09-03 02:09:07 -0300

    maven

---
+++ imprentaSocha/imprenta/imprenta/src/main/java/socha/tm/imprenta/ImprentaApplication.java
@@ -0,0 +1,13 @@
+package socha.tm.imprenta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImprentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImprentaApplication.class, args);
+	}
+
+}
---
+++ imprentaSocha/imprenta/imprenta/src/test/java/socha/tm/imprenta/ImprentaApplicationTests.java
@@ -0,0 +1,13 @@
+package socha.tm.imprenta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImprentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
